bef10ae2e5b5ff0a2a769d192502af8e4c7096b3
Tarun Gupta <99278334+WebxTechx@users.noreply.github.com>
2024-01-07 02:00:11 +0530

    admin dashboard work continue

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import { getPayloadClient } from "./get-payload";
+import { nextApp, nextHandler } from "./next-utils";
+
+const app = express();
+const PORT = Number(process.env.PORT) || 3200
+
+const start  = async () => {
+    const payload = await getPayloadClient({
+        initOptions:{
+            express:app,
+            onInit:async(cms) => {
+                cms.logger.info(`Admin URL ${cms.getAdminURL()}`)
+            }
+        }
+    })
+    app.use((req,res) => nextHandler(req,res))
+
+    nextApp.prepare().then(()=> {
+        payload.logger.info(`Next.Js started`)
+    
+        app.listen(PORT, async() => {
+            payload.logger.info(`Next.js APP URL ${process.env.NEXT_PUBLIC_SERVER_URL}`)
+        })
+    })
+}
+
+start();
